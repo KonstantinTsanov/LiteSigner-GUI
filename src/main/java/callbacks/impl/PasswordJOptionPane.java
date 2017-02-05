87934ef65481864f123248a30d4e506657c8c120
KTsan <KTsan@DESKTOP-EKFOIIF>
2017-02-05 12:51:09 +0200

    LiteSigner swing GUI. Initial Commit.

---
+++ src/main/java/callbacks/impl/PasswordJOptionPane.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package callbacks.impl;
+
+import callbacks.GuiPasswordCallback;
+import java.awt.Component;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+/**
+ *
+ * @author Konstantin Tsanov <k.tsanov@gmail.com>
+ */
+public class PasswordJOptionPane extends JOptionPane implements GuiPasswordCallback {
+
+    private final Component _parent;
+
+    public PasswordJOptionPane(Component parent) {
+        _parent = parent;
+    }
+
+    @Override
+    public char[] getPassword() {
+        String messageTitle = "Enter PIN";
+        JPasswordField password = new JPasswordField();
+        password.addAncestorListener(new AncestorListener() {
+            @Override
+            public void ancestorRemoved(final AncestorEvent event) {
+            }
+
+            @Override
+            public void ancestorMoved(final AncestorEvent event) {
+            }
+
+            @Override
+            public void ancestorAdded(final AncestorEvent event) {
+                password.requestFocusInWindow();
+            }
+        });
+
+        Object[] objs = {"Enter PIN:", password};
+        int result = JOptionPane.showConfirmDialog(_parent, objs, messageTitle, JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.PLAIN_MESSAGE, null);
+
+        while (result == JOptionPane.OK_OPTION && password.getPassword().length == 0) {
+            JOptionPane.showMessageDialog(_parent, "Please enter a valid password.", "Password is not valid",
+                    JOptionPane.ERROR_MESSAGE, null);
+            result = 0;
+            result = JOptionPane.showConfirmDialog(_parent, objs, messageTitle, JOptionPane.OK_CANCEL_OPTION,
+                    JOptionPane.PLAIN_MESSAGE, null);
+        }
+        return password.getPassword();
+    }
+}
---
+++ src/test/java/callbacks/impl/PasswordJOptionPaneTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package callbacks.impl;
+
+import java.io.File;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import pkcs.Pkcs11;
+import signers.Pkcs7;
+
+/**
+ *
+ * @author Konstantin Tsanov <k.tsanov@gmail.com>
+ */
+public class PasswordJOptionPaneTest {
+
+    public PasswordJOptionPaneTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPassword method, of class PasswordJOptionPane.
+     */
+    @Test
+    public void testGetPassword() throws InterruptedException {
+
+    }
+
+}
