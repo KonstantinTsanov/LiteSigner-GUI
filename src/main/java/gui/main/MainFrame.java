be318563316197bc356373839f37a78cfd01cf69
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-04 09:30:21 +0300

    Added actions to listeners.

--- src/main/java/gui/main/MainFrame.java
+++ src/main/java/gui/main/MainFrame.java
@@ -5,6 +5,7 @@
  */
 package gui.main;
 
+import gui.jpanels.ChooseAnOptionJPanel;
 import gui.jpanels.SelectingDeviceJPanel;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
@@ -15,56 +16,187 @@ import javax.swing.JSplitPane;
 import lombok.extern.java.Log;
 import net.miginfocom.swing.MigLayout;
 import org.usb4java.LibUsb;
+import callbacks.FrameControls;
+import enums.Languages;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  *
- * @author KTsan
+ * @author Konstantin Tsanov <k.tsanov@gmail.com>
  */
 @Log
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements FrameControls {
 
-    private CardLayout menuLayout, signingLayout;
-    private JPanel menuJPanel, signingJPanel;
+    private CardLayout deviceLayout, signingLayout;
+    private JPanel deviceJPanel, signingJPanel;
 
     private JSplitPane splitPaneH;
 
     private SelectingDeviceJPanel deviceSelectionPanel;
+    private ChooseAnOptionJPanel chooseAnOptionJPanel;
+
+    private final JMenuBar topMenuBar;
+    private JMenu fileJMenu, optionsJMenu, languageJMenu;
+    private JMenuItem exitJMenuItem;
 
     public MainFrame() {
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        initComponents();
-        attachListeners();
         setLayout(new MigLayout("", "[grow,fill]", "[grow,fill]"));
-        menuJPanel.setLayout(menuLayout);
+        setTitle("LiteSigner");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        topMenuBar = new JMenuBar();
+        initMenuBar();
+        initOptionsLayout();
+        initSigningLayout();
+        setLanguage(getLocaleFromPreferences());
+        add(chooseAnOptionJPanel);
+        pack();
+    }
+
+    private void initOptionsLayout() {
+        chooseAnOptionJPanel = new ChooseAnOptionJPanel(this);
+    }
+
+    private void initSigningLayout() {
+        deviceLayout = new CardLayout();
+        signingLayout = new CardLayout();
+        deviceJPanel = new JPanel();
+        signingJPanel = new JPanel();
+        splitPaneH = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        deviceSelectionPanel = new SelectingDeviceJPanel(this);
+        attachSigningListeners();
+        deviceJPanel.setLayout(deviceLayout);
         signingJPanel.setLayout(signingLayout);
         signingJPanel.add(deviceSelectionPanel, deviceSelectionPanel.getClass().toString());
         splitPaneH.setDividerLocation(0.5);
         splitPaneH.setResizeWeight(0.5);
-        add(splitPaneH, BorderLayout.CENTER);
         splitPaneH.setLeftComponent(signingJPanel);
-        splitPaneH.setRightComponent(menuJPanel);
+        splitPaneH.setRightComponent(deviceJPanel);
+    }
+
+    @Override
+    public void showSigningLayout() {
+        this.getContentPane().remove(chooseAnOptionJPanel);
+        add(splitPaneH, BorderLayout.CENTER);
         pack();
+        revalidate();
+        repaint();
     }
 
-    private void initComponents() {
-        menuLayout = new CardLayout();
-        signingLayout = new CardLayout();
-        menuJPanel = new JPanel();
-        signingJPanel = new JPanel();
-        splitPaneH = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        deviceSelectionPanel = new SelectingDeviceJPanel(this);
+    @Override
+    public void showChooseOptionLayout() {
+        this.getContentPane().remove(splitPaneH);
+        add(chooseAnOptionJPanel, BorderLayout.CENTER);
+        pack();
+        revalidate();
+        repaint();
     }
 
-    private void attachListeners() {
-        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    private void attachSigningListeners() {
         this.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent winEvt) {
                 deviceSelectionPanel.cancelDeviceScanner();
-                LibUsb.exit(null);
+                try {
+                    LibUsb.exit(null);
+                } catch (IllegalStateException ex) {
+                    //I dont care, the default context has not been initialized in the first place.
+                }
                 System.exit(0);
             }
         });
+    }
+
+    private void initMenuBar() {
+        createInitFileMenu();
+        createInitOptionsMenu();
+        setJMenuBar(topMenuBar);
+    }
+
+    /**
+     * creates and initializes the file menu and submenus
+     */
+    private void createInitFileMenu() {
+        fileJMenu = new JMenu();
+        fileJMenu.setMnemonic(KeyEvent.VK_F);
+        exitJMenuItem = new JMenuItem("Exit");
+        exitJMenuItem.setMnemonic(KeyEvent.VK_E);
+        exitJMenuItem.setToolTipText("Exit application");
+        exitJMenuItem.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+        fileJMenu.add(exitJMenuItem);
+        topMenuBar.add(fileJMenu);
+    }
+
+    /**
+     * Creates and initizlies the options menu and submenu
+     */
+    private void createInitOptionsMenu() {
+        optionsJMenu = new JMenu();
+        optionsJMenu.setMnemonic(KeyEvent.VK_O);
+        languageJMenu = new JMenu();
+        for (Languages lang : Languages.values()) {
+            JMenuItem language = new JMenuItem(lang.getName());
+            language.addActionListener((ActionEvent ActionEvent) -> {
+                if (getLocaleFromPreferences() != lang.getLocale()) {
+                    setLanguage(lang.getLocale());
+                    setLocaleToPreferences(lang);
+                    pack();
+                }
+            });
+            languageJMenu.add(language);
+        }
+        optionsJMenu.add(languageJMenu);
+        topMenuBar.add(optionsJMenu);
+    }
+
+    private void setLanguage(Locale locale) {
+        deviceSelectionPanel.setComponentText(locale);
+        chooseAnOptionJPanel.setComponentText(locale);
+        setComponentText(locale);
+    }
+
+    /**
+     * Gets the lastly used language from the registry(windows) or whatever it
+     * is in linux
+     *
+     * @return new locale object, composed of the extracted parameters
+     */
+    private Locale getLocaleFromPreferences() {
+        Preferences prefs = Preferences.userRoot().node(getClass().getName());
+        String language = "Language";
+        String country = "Country";
+        return new Locale(prefs.get(language, "en"), prefs.get(country, "US"));
+    }
+
+    /**
+     * Sets the lastly used language in the registry(windows) or whatever it is
+     * in linux
+     *
+     * @param lang enum containing the required variables
+     */
+    private void setLocaleToPreferences(Languages lang) {
+        Preferences prefs = Preferences.userRoot().node(getClass().getName());
+        String language = "Language";
+        String country = "Country";
+        prefs.put(language, lang.getShortLanguage());
+        prefs.put(country, lang.getShortCountry());
+    }
+
+    private void setComponentText(Locale locale) {
+        ResourceBundle r = ResourceBundle.getBundle("Bundle", locale);
+        fileJMenu.setText(r.getString("MainFrame.optionsMenu.fileJMenu"));
+        exitJMenuItem.setText(r.getString("MainFrame.optionsMenu.exitJMenuItem"));
+        optionsJMenu.setText(r.getString("MainFrame.optionsMenu.optionsJMenu"));
+        languageJMenu.setText(r.getString("MainFrame.optionsMenu.languageJMenu"));
     }
 
     public static void main(String args[]) {
