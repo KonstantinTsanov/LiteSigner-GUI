1e543fa2b9d46468f02a5620dd879a6d0f71fcd2
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-04 09:29:57 +0300

    Added panels

---
+++ src/main/java/gui/jpanels/SelectingCertificateJPanel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.jpanels;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Konstantin Tsanov <k.tsanov@gmail.com>
+ */
+public class SelectingCertificateJPanel extends JPanel {
+
+    private JLabel selectingCertificateJLabel;
+    
+}
