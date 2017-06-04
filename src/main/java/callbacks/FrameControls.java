906d856a2414430b646ea6b44bb118bb4349a79f
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-04 09:32:54 +0300

    Added interface to allow the jpanels to have control over the parent jframe.

---
+++ src/main/java/callbacks/FrameControls.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package callbacks;
+
+/**
+ *
+ * @author Konstantin Tsanov <k.tsanov@gmail.com>
+ */
+public interface FrameControls {
+
+    public void showSigningLayout();
+
+    public void showChooseOptionLayout();
+}
