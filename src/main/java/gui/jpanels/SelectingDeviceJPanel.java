25dc6b342e871662ebe4c0afd7d51f24b5cc851b
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-06 09:11:27 +0300

    Refactored method. Moved threads to separate class.

--- src/main/java/gui/jpanels/SelectingDeviceJPanel.java
+++ src/main/java/gui/jpanels/SelectingDeviceJPanel.java
@@ -6,17 +6,26 @@
 package gui.jpanels;
 
 import callbacks.FrameControls;
+import callbacks.SelectingDeviceJPanelControls;
+import java.awt.Color;
+import java.awt.Component;
 import litesigner.callbacks.impl.PasswordJOptionPane;
 import java.io.File;
+import java.security.KeyStoreException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -33,69 +42,73 @@ import net.miginfocom.swing.MigLayout;
 import pkcs.Pkcs11;
 import sun.security.pkcs11.wrapper.PKCS11Exception;
 import tools.DeviceManager;
+import tools.LiteSignerManager;
 
 /**
  *
  * @author Konstantin Tsanov <k.tsanov@gmail.com>
  */
 @Log
-public class SelectingDeviceJPanel extends JPanel {
-
+public class SelectingDeviceJPanel extends JPanel implements SelectingDeviceJPanelControls {
+    
     private JLabel selectingDeviceJLabel;
     private JButton logInDeviceJButton;
     private JButton backJButton;
-    Map<String, Map.Entry<Integer, File>> tokensDescription;
-    DefaultListModel<String> model = new DefaultListModel<>();
+    //Must be able to control both description and the entry containing slotIndex and driver
+    Map<String, Map.Entry<Integer, File>> tokensList;
+    //Contains ONLY descriptions
+    DefaultListModel<String> tokensModel = new DefaultListModel<>();
     private JList<String> deviceList;
     private JScrollPane deviceScrollPane;
-    private final ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
     private final JFrame parent;
-
+    
+    
     public SelectingDeviceJPanel(JFrame parent) {
         this.parent = parent;
-        tokensDescription = new HashMap<>();
+        tokensList = new HashMap<>();
         MigLayout layout = new MigLayout("", "[grow][grow]", "[shrink 0][grow][shrink 0]");
         setLayout(layout);
         initComponents();
         addComponents();
         attachListeners();
-
+        
     }
-
+    
     private void initComponents() {
         selectingDeviceJLabel = new JLabel();
         logInDeviceJButton = new JButton();
         backJButton = new JButton();
-        deviceList = new JList<>(model);
+        deviceList = new JList<>(tokensModel);
         deviceScrollPane = new JScrollPane(deviceList);
     }
-
+    
     private void addComponents() {
         add(selectingDeviceJLabel, "span");
         add(deviceScrollPane, "grow, span, wrap");
         add(backJButton, "left");
         add(logInDeviceJButton, "span 2, right");
     }
-
+    
     public void setComponentText(Locale locale) {
         ResourceBundle r = ResourceBundle.getBundle("Bundle", locale);
         selectingDeviceJLabel.setText(r.getString("selectingDeviceJPanel.selectDeviceLabel"));
         logInDeviceJButton.setText(r.getString("selectingDeviceJPanel.logInButton"));
         backJButton.setText(r.getString("selectingDeviceJPanel.backJButton"));
     }
-
+    
     private void attachListeners() {
         this.addAncestorListener(new AncestorListener() {
             @Override
             public void ancestorAdded(AncestorEvent event) {
-                displayDevices();
+                //setting the scanner to check for devices every 1 second
+                LiteSignerManager.getInstance().runDeviceScanner(1);
             }
-
+            
             @Override
             public void ancestorRemoved(AncestorEvent ae) {
-
+                
             }
-
+            
             @Override
             public void ancestorMoved(AncestorEvent ae) {
             }
@@ -106,61 +119,34 @@ public class SelectingDeviceJPanel extends JPanel {
         logInDeviceJButton.addActionListener((al) -> {
             String selectedFromTheList = deviceList.getSelectedValue();
             if (selectedFromTheList != null) {
-                Map.Entry<Integer, File> selectedDevice = tokensDescription.get(selectedFromTheList);
-                System.out.println(selectedFromTheList);
-                //[slotListIndex][driver]
-                Pkcs11 smartcard = new Pkcs11(selectedDevice.getKey(), selectedDevice.getValue());
-                Thread thread1 = new Thread(() -> {
-                    smartcard.initGuiHandler(new PasswordJOptionPane(null));
-                    smartcard.login();
-                });
-                thread1.start();
+                deviceLogIn(selectedFromTheList);
             }
         });
-
+        
     }
 
-    private void displayDevices() {
-        Thread t = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Map<String, Map.Entry<Integer, File>> devices = DeviceManager.getInstance().scanForUSBDevices();
-                    if (devices != null) {
-                        SwingUtilities.invokeLater(() -> {
-                            devices.forEach((description, indexAndDriver) -> {
-                                if (tokensDescription.containsKey(description) == false) {
-                                    tokensDescription.put(description, indexAndDriver);
-                                    model.addElement(description);
-                                }
-                            });
-                            for (Iterator<Map.Entry<String, Map.Entry<Integer, File>>> it = tokensDescription.entrySet().iterator(); it.hasNext();) {
-                                String description = it.next().getKey();
-                                if (devices.containsKey(description) == false) {
-                                    it.remove();
-                                    model.removeElement(description);
-                                }
-                            }
-                        });
-                    }
-                } catch (PKCS11Exception ex) {
-                    SwingUtilities.invokeLater(() -> {
-                        JOptionPane.showMessageDialog(parent, "There was a problem with the device.");
-                    });
-                }
-            }
-        };
-        exec.scheduleAtFixedRate(t, 0, 1, TimeUnit.SECONDS);
-        t.start();
-    }
-
-    public boolean cancelDeviceScanner() {
-        exec.shutdown();
+    //TODO work on it
+    private void deviceLogIn(String selectedFromTheList) {
+        Entry<Integer, File> selectedDevice = tokensList.get(selectedFromTheList);
+        Pkcs11 smartcard = new Pkcs11(selectedDevice.getKey(), selectedDevice.getValue());
+        smartcard.initGuiHandler(new PasswordJOptionPane(null));
         try {
-            return exec.awaitTermination(3, TimeUnit.SECONDS);
-        } catch (InterruptedException ex) {
-            log.log(Level.SEVERE, "Failed to clean things up before closing the application!", ex);
-            return false;
+            smartcard.login();
+           // tokensModel.setElementAt(tokensModel.elementAt(tokensModel.indexOf(selectedFromTheList)).concat(loggedIn), tokensModel.indexOf(selectedFromTheList));
+        } catch (KeyStoreException ex) {
+            SwingUtilities.invokeLater(() -> {
+                JOptionPane.showMessageDialog(parent, "There is a problem with the device.");
+            });
         }
+    }
+    
+    @Override
+    public DefaultListModel<String> getTokensModel() {
+        return tokensModel;
+    }
+    
+    @Override
+    public JFrame getParent() {
+        return parent;
     }
 }
